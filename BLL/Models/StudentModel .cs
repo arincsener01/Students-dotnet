15ea471703fa087e5485e6f822c746cd758d2ff9
Arinc Sener <senerar-nc@windowslive.com>
2024-10-07 00:14:41 +0300

    Add project files.

---
+++ BLL/Models/StudentModel .cs
@@ -0,0 +1,23 @@
+﻿using BLL.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Models
+{
+    public class StudentModel
+    {
+        public Student Record{get;set;}
+        //public string Name{get;set;}
+        //public string Surname { get;set;}
+        //public DateTime BirthDate {  get;set;}
+        //public decimal GPA { get;set;}
+
+        public string Name => Record.Name;
+        public string Surname => Record.Surname;
+        public DateTime? BirthDate => Record.BirthDate;
+        public decimal? GPA => Record.Gpa;
+    }
+}
